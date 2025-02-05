db146a14274b206cc2d81039092ff2b3eb8f9f5a
yojanathapa2 <yojanathapa122@gmail.com>
2025-02-06 00:29:11 +0545

    changed image

--- Monkey-Maze/js/sorting.js
+++ Monkey-Maze/js/sorting.js
@@ -8,18 +8,20 @@ for (let i = 255; i <= 3825; i = i + 255) {
 }
 
 function shuffle(array) {
-  let currentIndex = array.length,  randomIndex;
+  let currentIndex = array.length,
+    randomIndex;
 
   // While there remain elements to shuffle...
   while (currentIndex != 0) {
-
     // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
 
     // And swap it with the current element.
     [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
+      array[randomIndex],
+      array[currentIndex],
+    ];
   }
   return array;
 }
@@ -27,38 +29,34 @@ function shuffle(array) {
 function generatearray() {
   var newArray = shuffle(array);
   for (var i = 0; i < 15; i++) {
-
     var value = newArray[i];
 
     var array_ele = document.createElement("div");
     array_ele.classList.add("block");
     array_ele.style.height = `${value * 0.1}px`;
     array_ele.style.transform = `translate(${i * 50}px)`;
-  
+
     // Creating label element for displaying size of particular block
-    var array_ele_label = 
-    document.createElement("label");
+    var array_ele_label = document.createElement("label");
     array_ele_label.classList.add("block_id");
     array_ele_label.innerText = value;
-  
+
     array_ele.appendChild(array_ele_label);
     container.appendChild(array_ele);
   }
 }
 
 // to generate indexes
-var count_container = 
-document.getElementById("count");
+var count_container = document.getElementById("count");
 function generate_idx() {
   for (var i = 0; i < 15; i++) {
     var array_ele2 = document.createElement("div");
     array_ele2.classList.add("block2");
     array_ele2.style.height = `${20}px`;
     array_ele2.style.transform = `translate(${i * 50}px)`;
-  
+
     // Adding indexes
-    var array_ele_label2 = 
-    document.createElement("label");
+    var array_ele_label2 = document.createElement("label");
     array_ele_label2.classList.add("block_id3");
     array_ele_label2.innerText = i;
 
@@ -66,7 +64,6 @@ function generate_idx() {
     count_container.appendChild(array_ele2);
   }
 }
-  
 
 generatearray();
 generate_idx();
